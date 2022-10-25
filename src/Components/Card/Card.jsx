99c6a8a9d1968cfe3fe3d6c5d5f00d0cf9c06147
dalismiles <dali.iapichino@gmail.com>
2022-10-25 18:48:03 +0200

    fix:minor fix

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,10 +1,21 @@
 import "./index.css";
 import Button from "../Button/Button.jsx";
+import ImageMob from "../../images/image-product-mobile.jpg";
+import ImageDesk from "../../images/image-product-desktop.jpg";
 
 const Card = () => {
   return (
     <div className="Card">
-      <div className="Card__img"></div>
+      <div>
+        <picture>
+          <source media="(min-width: 1440px)" srcSet={ImageDesk} />
+          <img
+            src={ImageMob}
+            alt="Photo of a bottle of Gabrielle Parfum"
+            style={{ width: "100%" }}
+          />
+        </picture>
+      </div>
       <div className="Card__body">
         <p className="Card__body-subtitle">PERFUME</p>
         <h1 className="Card__body-title">Gabrielle Essence Eau De Parfum</h1>
@@ -14,7 +25,9 @@ const Card = () => {
         </p>
         <div className="Card__body-price">
           <p className="Card__body-new-price">$149.99</p>
-          <p className="Card__body-old-price">$169.99</p>
+          <del>
+            <p className="Card__body-old-price">$169.99</p>
+          </del>
         </div>
         <Button />
       </div>
